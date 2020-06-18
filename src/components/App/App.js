e52efb2c3a776ab72549f2b6470fbdb961f7bf85
Jake Dahl <jakedahl16@yahoo.com>
2020-06-18 11:47:43 -0500

    switched input, label, button to search.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,22 +2,7 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
-searchGif = () => {
 
-}
-
-
-
-  render() {
-    return (
-      <div>
-        <h1>Giphy Search!</h1>
-        <label for='searchBar'>Search for gif: </label>
-        <input onChange={this.searchGif} id='searchBar' type='text'/>
-        <button onClick={this.findGif} id='submitSearch' >Find gif!</button>
-      </div>
-    );
-  }
   
 }
 
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Search extends Component {
+    searchGif = () => {
+
+    }
+
     render(){
         return(
-            <div></div>
+            <div>
+                <div>
+                    <h1>Giphy Search!</h1>
+                    <label for='searchBar'>Search for gif: </label>
+                    <input onChange={this.searchGif} id='searchBar' type='text' />
+                    <button onClick={this.findGif} id='submitSearch' >Find gif!</button>
+                </div>
+            </div>
         )
     }
 }
