d7953631508a184a890db96aaff733c65f51a085
Jake Dahl <jakedahl16@yahoo.com>
2020-06-18 13:24:13 -0500

    get request and input works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-
+import Search from '../Search/Search';
+import { connect } from 'react-redux';
 
 class App extends Component {
-
-  
+  render() {
+    return (
+      <div>
+        <h1>Giphy Search!</h1>
+        <Search />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Search extends Component {
-    searchGif = () => {
 
+    state = {
+        newSearch: ''
+    }
+
+    searchGif = (event) => {
+        this.setState({
+            newSearch: {
+                ...this.state.newSearch,
+                newSearch: event.target.value
+
+            }
+        })
+        
+    }
+
+    findGif = (event) => {
+        event.preventDefault();
+        console.log('this is the findGif', this.state.newSearch);
+        this.props.dispatch({ type: 'GET_SEARCH', payload: this.state.newSearch })
     }
 
     render(){
@@ -12,7 +30,7 @@ class Search extends Component {
                 <div>
                     <h1>Giphy Search!</h1>
                     <label for='searchBar'>Search for gif: </label>
-                    <input onChange={this.searchGif} id='searchBar' type='text' />
+                    <input onChange={this.searchGif} id='searchBar' type='text' value= {this.props.newSearch}/>
                     <button onClick={this.findGif} id='submitSearch' >Find gif!</button>
                 </div>
             </div>
