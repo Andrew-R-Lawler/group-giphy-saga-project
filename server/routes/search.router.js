6c9c8c7c1d278a4f2434b3381d5d6834febc9d5c
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-18 12:00:24 -0500

    server.js updates

---
+++ server/routes/search.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { default: axios } = require('axios');
+const router = express.Router();
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const axios = require('axios');
+require('dotenv').config();
 
 // Route includes
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
-
+const searchRouter = require('./routes/search.router')
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,7 +18,9 @@ app.use(express.static('build'));
 /* Routes */
 app.use('/api/favorite', favoriteRouter);
 app.use('/api/category', categoryRouter);
+app.use('/api/search')
 
+axios.get(`http://`)
 // App Set //
 const PORT = process.env.PORT || 5000;
 
