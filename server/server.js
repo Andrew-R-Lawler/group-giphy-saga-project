0d10403f446097dcb673a745faf9520ec9c92799
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-18 13:16:01 -0500

    server.js updates

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
 const searchRouter = require('./routes/search.router')
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +19,7 @@ app.use(express.static('build'));
 /* Routes */
 app.use('/api/favorite', favoriteRouter);
 app.use('/api/category', categoryRouter);
-app.use('/api/search')
+app.use('/search', searchRouter);
 
 axios.get(`http://`)
 // App Set //
