4b7fb17da354ed4919e03ac6303ec106bfef6115
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-18 14:58:36 -0500

    /favorite GET request endpoint added to favorite.router

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,14 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  console.log('GET /api/favorite');
+  pool.query('SELECT * FROM "favorites";')
+    .then((result) => {
+      res.send(result.rows);
+    }).catch((error) => {
+      console.log('Error GET /api/favorite');
+      res.sendStatus(500);
+    })
 });
 
 // add a new favorite 
