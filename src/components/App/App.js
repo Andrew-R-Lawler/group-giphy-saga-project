d9b6069bed3ae5d1556021da7e1fa3d1f81efa0a
Jake Dahl <jakedahl16@yahoo.com>
2020-06-19 11:47:40 -0500

    added a nav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Search from '../Search/Search';
 import { connect } from 'react-redux';
+import FavoriteView from '../FavoriteView/FavoriteView';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 
@@ -8,7 +10,25 @@ class App extends Component {
     return (
       <div>
         <h1>Giphy Search!</h1>
-        <Search />
+        {/* <Search /> */}
+        <Router>
+          <div className= 'navigationBar'></div>
+          <ul className='navBar'>
+            <li>
+              <Link to = '/FavoriteView'>Favorites</Link>
+            </li>
+            <li>
+              <Link to='/Search'>
+                Search
+            </Link>
+            </li>
+          </ul>
+          
+           
+          
+          <Route path='/Search' component={Search} />
+          <Route path='/FavoriteView' component={FavoriteView} />
+        </Router>
       </div>
     );
   }
