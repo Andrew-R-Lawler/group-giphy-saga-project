5117cfd5cecee61e3ba6d9f30f0b5194f84c10b2
Tayasir Dahir <tdahir717@gmail.com>
2020-06-19 11:21:05 -0500

    favorites

--- src/components/FavoriteView/FavoriteView.js
+++ src/components/FavoriteView/FavoriteView.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class FavoriteView extends Component {
+
+    state = {
+        favorite: '',
+    }
+
+    componentDidMount(){
+        this.getFavorites();
+    }
+
+    getFavorites = () => {
+        this.props.dispatch({
+            type: 'SET_FAVORITES'
+        })
+    }
+    
     render() {
         return (
-            <div></div>
+            <div>
+                <h2>Favorites List</h2>
+                {this.props.reduxState.favoriteReducer.map(item =>
+                    <select>
+                        <option value="funny">Funny</option>
+                        <option value="cohort">Cohort</option>
+                        <option value="cartoon">Cartoon</option>
+                        <option value="nsfw">NSFW</option>
+                        <option value="meme">Meme</option>
+                    </select>
+                    )}
+            </div>
         )
     }
 }
